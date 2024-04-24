cd3a2c028d1c0123da5a94bf46bc43057e0acd60
Gagan714 <gaganursmg@gmail.com>
2024-04-24 13:19:21 +0530

    manager design

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
+import Manager from './components/Manager'
 
 function App() {
   return (
     <>
     <Navbar/>
-      <div>my ui</div>
+    <Manager/>
     </>
   )
 }
---
+++ src/components/Manager.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Manager = () => {
+  return (
+    <div className="container flex justify-center items-center">
+    <div className="bg-slate-500 max-w-lg">
+        <div className="flex-col">
+        <div className="logo font-bold text-xl">
+            <span className=" text-gray-800">&lt;</span>
+            <span>Secret</span>
+            <span className="text-gray-800">Shield/&gt;</span>
+            </div>
+            <div>Your Personal Password Manager</div>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default Manager
