c04f1075cdbf06dbe2607f5227614a0faa455a1b
Gagan714 <gaganursmg@gmail.com>
2024-04-24 23:45:06 +0530

    done for the day

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
+import React from 'react';
 
 const Manager = () => {
   return (
-    <div className="container flex justify-center items-center">
-    <div className="bg-slate-500 max-w-lg">
-        <div className="flex-col">
-        <div className="logo font-bold text-xl">
-            <span className=" text-gray-800">&lt;</span>
-            <span>Secret</span>
-            <span className="text-gray-800">Shield/&gt;</span>
-            </div>
-            <div>Your Personal Password Manager</div>
+    <div className="flex justify-center items-center my-2">
+      <div className="bg-gray-500 w-2/3 flex flex-col justify-center items-center text-center text-white rounded-xl">
+        <h1 className="font-semibold text-3xl"><span>Secret</span><span className="text-gray-800">Shield</span></h1>
+        <p className="text-gray-900">Your Personal Password Manager</p>
+        <input className="border border-black rounded-2xl my-6 mx-4 w-4/5" type="text" />
+        <div className="flex gap-56 justify-around py-3">
+          <input className="rounded-2xl border border-black" type="text" />
+          <input className="rounded-2xl border border-black" type="text" />
         </div>
+        <button className="flex justify-center items-center my-2 border-black bg-blue-700 text-black px-3 rounded-xl">
+          Save Password
+          <lord-icon
+            src="https://cdn.lordicon.com/jgnvfzqg.json"
+            trigger="hover"
+            style={{ width: '20px', height: '20px' }}>
+          </lord-icon>
+        </button>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Manager
+export default Manager;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,8 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className="container flex justify-around items-center py-1 bg-orange-500 text-slate-100">
-        <div className="logo font-bold text-2xl">
-            <span className=" text-gray-800">&lt;</span>
-            <span>Secret</span>
-            <span className="text-gray-800">Shield/&gt;</span>
-            </div>
+    <div className="container flex justify-around items-center py-1 bg-gray-700 text-slate-100">
+        <div className="logo font-bold text-2xl"><span>Secret</span><span className=" text-gray-900">Shield</span></div>
         <ul className="flex gap-3">
             <li><a href="/">Home</a></li>
             <li><a href="/">About</a></li>
