2f05e08828d4244f3d743abe501e41355e2be23b
Gagan714 <gaganursmg@gmail.com>
2024-04-26 15:17:23 +0530

    color changes

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -4,12 +4,17 @@ const Manager = () => {
   return (
     <div className="flex justify-center items-center my-2">
       <div className="w-2/3 flex flex-col justify-center items-center text-center text-white rounded-xl">
-        <h1 className="font-semibold text-3xl text-black"><span>Secret</span><span className="text-blue-500">Shield</span></h1>
+        <h1 className="font-semibold text-3xl text-black"><span>Secret</span><span className="text-blue-700">Shield</span></h1>
         <p className="text-gray-900">Your Personal Password Manager</p>
         <input placeholder='Enter website URL' className="border border-black rounded-3xl my-6 mx-4 w-4/5 px-2 py-1 text-black" type="text" />
         <div className="flex w-full justify-around py-3">
-          <input placeholder='Enter Username' className="rounded-3xl border border-black px-2 py-1 text-black" type="text" />
-          <input placeholder='Enter Password' className="rounded-3xl border border-black px-2 py-1 text-black" type="text" />
+          <div>
+            <input placeholder='Enter Username' className="rounded-3xl border border-black px-2 py-1 text-black" type="text" />
+            </div>
+          <div className="relative">
+            <input placeholder='Enter Password' className="rounded-3xl border border-black px-2 py-1 text-black" type="text" />
+            <img src="" className="absolute right-0 top-0" alt="" />
+            </div>
         </div>
         <button className="flex justify-center items-center my-2 py-1 border-black bg-blue-500 text-black px-3 rounded-2xl">
           Save Password
