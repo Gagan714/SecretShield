f9babdabf3e056e55409560c25e7b893606281b1
Gagan714 <gaganursmg@gmail.com>
2024-04-26 14:59:01 +0530

    ui changes

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -3,15 +3,15 @@ import React from 'react';
 const Manager = () => {
   return (
     <div className="flex justify-center items-center my-2">
-      <div className="bg-gray-500 w-2/3 flex flex-col justify-center items-center text-center text-white rounded-xl">
-        <h1 className="font-semibold text-3xl"><span>Secret</span><span className="text-gray-800">Shield</span></h1>
+      <div className="w-2/3 flex flex-col justify-center items-center text-center text-white rounded-xl">
+        <h1 className="font-semibold text-3xl text-black"><span>Secret</span><span className="text-blue-500">Shield</span></h1>
         <p className="text-gray-900">Your Personal Password Manager</p>
-        <input className="border border-black rounded-2xl my-6 mx-4 w-4/5" type="text" />
-        <div className="flex gap-56 justify-around py-3">
-          <input className="rounded-2xl border border-black" type="text" />
-          <input className="rounded-2xl border border-black" type="text" />
+        <input placeholder='Enter website URL' className="border border-black rounded-3xl my-6 mx-4 w-4/5 px-2 py-1 text-black" type="text" />
+        <div className="flex w-full justify-around py-3">
+          <input placeholder='Enter Username' className="rounded-3xl border border-black px-2 py-1 text-black" type="text" />
+          <input placeholder='Enter Password' className="rounded-3xl border border-black px-2 py-1 text-black" type="text" />
         </div>
-        <button className="flex justify-center items-center my-2 border-black bg-blue-700 text-black px-3 rounded-xl">
+        <button className="flex justify-center items-center my-2 py-1 border-black bg-blue-500 text-black px-3 rounded-2xl">
           Save Password
           <lord-icon
             src="https://cdn.lordicon.com/jgnvfzqg.json"
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className="container flex justify-around items-center py-1 bg-gray-700 text-slate-100">
-        <div className="logo font-bold text-2xl"><span>Secret</span><span className=" text-gray-900">Shield</span></div>
+    <div className="container flex justify-around items-center py-1 bg-black text-slate-100">
+        <div className="logo font-bold text-2xl"><span>Secret</span><span className=" text-blue-500">Shield</span></div>
         <ul className="flex gap-3">
             <li><a href="/">Home</a></li>
             <li><a href="/">About</a></li>
