01a8e92a30326051bf8cad6174115b213fdc2268
Gagan714 <gaganursmg@gmail.com>
2024-04-27 18:18:39 +0530

    passwords file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Manager from './components/Manager'
+import passwords from './components/passwords'
 
 function App() {
   return (
     <>
     <Navbar/>
     <Manager/>
+    <passwords/>
     </>
   )
 }
---
+++ src/components/passwords.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const passwords = () => {
+  return (
+    <div className="passwords">
+        <h2>Your Passwords</h2>
+      </div>
+  )
+}
+
+export default passwords
