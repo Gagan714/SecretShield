10354330fe5330f11242424e85ccbe4c7cd9366c
Gagan714 <gaganursmg@gmail.com>
2024-04-28 17:25:23 +0530

    github

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className="container flex justify-around items-center py-1 bg-black text-slate-100">
+    <div className="container flex justify-around items-center py-1 bg-blue-900 text-slate-100">
         <div className="logo font-bold text-2xl"><span>Secret</span><span className=" text-blue-500">Shield</span></div>
-        <ul className="flex gap-3">
-            <li><a href="/">Home</a></li>
-            <li><a href="/">About</a></li>
-            <li><a href="/">Contact me</a></li>
-        </ul>
+        <a href="https://github.com/Gagan714/SecretShield" target="_blank"><button className="bg-black text-white rounded-3xl border-2 border-black flex gap-2 py-1 px-3 items-center">
+          Github
+          <img className="invert w-6 h-7" src="src/assets/github.png" alt="" />
+        </button></a>
     </div>
   )
 }
