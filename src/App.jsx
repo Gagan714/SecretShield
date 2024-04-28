4967c3cfafa54ddd14d5483b1cf945255ff651d3
Gagan714 <gaganursmg@gmail.com>
2024-04-28 16:51:11 +0530

    table to show passwords

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Manager from './components/Manager'
-import Passwords from './components/Passwords'
 
 function App() {
   return (
     <>
     <Navbar/>
     <Manager/>
-    <Passwords/>
     </>
   )
 }
--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -17,16 +17,19 @@ const Manager = () => {
     setShowPassword(!showPassword);
   };
   const savePassword=()=>{
-    console.log(form)
+    if (form.site.trim() === "" || form.username.trim() === "" || form.password.trim() === "") {
+      alert("Please fill in all fields");
+      return;
+    }
     setpasswordarray([...passwordarray,form])
     localStorage.setItem("passwords",JSON.stringify([...passwordarray,form]))
-    console.log([...passwordarray,form])
   }
   const handleChange=(e)=>{
     setform({...form,[e.target.name]:e.target.value})
   }
 
   return (
+    <div>
     <div className="flex justify-center items-center my-2">
       <div className="w-2/3 flex flex-col justify-center items-center text-center text-white rounded-xl">
         <h1 className="font-semibold text-3xl text-black">
@@ -56,6 +59,32 @@ const Manager = () => {
         </button>
       </div>
     </div>
+    <div className="flex justify-center items-center my-2">
+    <div className="w-2/3 flex flex-col justify-center items-center text-center text-black rounded-xl">
+    <h2 className="font-bold text-2xl ">Your Passwords</h2>
+    {passwordarray.length===0 && <div>No passwords to show</div>}
+    {passwordarray.length!=0 && <table class="table-auto w-full rounded-md overflow-hidden">
+<thead className="bg-blue-500 text-black">
+<tr>
+  <th className="py-2">Website</th>
+  <th className="py-2">Username</th>
+  <th className="py-2">Password</th>
+</tr>
+</thead>
+<tbody className="bg-blue-100">
+{passwordarray.map((item,index)=>{
+  return <tr key={index}>
+  <td className="text-center py-2 border border-white w-32"><a href={item.site} target='_blank'>{item.site}</a></td>
+  <td className="text-center py-2 border border-white w-32">{item.username}</td>
+  <td className="text-center py-2 border border-white w-32">{item.password}</td>
+</tr>
+})}
+
+</tbody>
+</table>}
+</div>
+</div>
+</div>
   );
 }
 
