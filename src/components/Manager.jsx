35261faf58154701bec43e3ff3e37a92a1d68a7a
Gagan714 <gaganursmg@gmail.com>
2024-04-29 11:01:37 +0530

    responsive

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -101,20 +101,20 @@ transition="Bounce"
 {/* Same as */}
 <ToastContainer />
     <div className="flex justify-center items-center my-2">
-      <div className="w-2/3 flex flex-col justify-center items-center text-center text-white rounded-xl">
+      <div className="w-2/3 flex flex-col  justify-center items-center text-center text-white rounded-xl">
         <h1 className="font-semibold text-3xl text-black">
           <span>Secret</span>
           <span className="text-blue-700">Shield</span>
         </h1>
         <p className="text-gray-900">Your Personal Password Manager</p>
         <input value={form.site} name="site" placeholder='Enter website URL' onChange={handleChange} className="border border-black rounded-3xl my-6 mx-4 w-4/5 px-2 py-1 text-black" type="text" />
-        <div className="flex w-full justify-around py-3">
+        <div className="flex md:flex-row flex-col w-full  justify-around py-3">
           <div>
-            <input value={form.username} name="username" placeholder='Enter Username' onChange={handleChange} className="rounded-3xl border border-black px-2 py-1 text-black" type="text" />
+            <input value={form.username} name="username" placeholder='Enter Username' onChange={handleChange} className="rounded-3xl border border-black px-2 py-1 text-black md:w-full" type="text" />
           </div>
-          <div className="relative">
-            <input value={form.password} name="password" placeholder='Enter Password' onChange={handleChange} className="rounded-3xl border border-black px-2 py-1 text-black" type={showPassword ? 'text' : 'password'} ref={passwordRef} />
-            <div className="absolute right-2 top-2 h-6 w-5 cursor-pointer" onClick={togglePasswordVisibility}>
+          <div className="flex gap-2 justify-center items-center ml-6 pt-4 md:pt-0">
+            <div><input value={form.password} name="password" placeholder='Enter Password' onChange={handleChange} className="rounded-3xl border border-black px-2 py-1 text-black" type={showPassword ? 'text' : 'password'} ref={passwordRef} /></div>
+            <div className="h-6 w-5 cursor-pointer" onClick={togglePasswordVisibility}>
               <img src={showPassword ? "src/assets/eyecross.png" : "src/assets/eye.png"} alt="Toggle Password Visibility" />
             </div>
           </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className="container flex justify-around items-center py-1 bg-blue-900 text-slate-100">
+    <div className="flex justify-around items-center py-1 bg-blue-900 text-slate-100 w-full">
         <div className="logo font-bold text-2xl"><span>Secret</span><span className=" text-blue-500">Shield</span></div>
         <a href="https://github.com/Gagan714/SecretShield" target="_blank"><button className="bg-black text-white rounded-3xl border-2 border-black flex gap-2 py-1 px-3 items-center">
           Github
